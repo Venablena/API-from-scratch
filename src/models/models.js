30632f6323fc79080238d6235e4671e341b26d12
Lena Venable <venablena@gmail.com>
2017-10-22 19:44:16 -0700

    error handling for item creation complete

--- src/models/models.js
+++ src/models/models.js
@@ -2,37 +2,20 @@ const uuid = require('uuid')
 const curtains = []
 
 function create(body) {
-  const model = body.model
-  const color = body.color
-  const size = body.size
-  const room = body.room
-
-  const errors = []
-
+  const errors = checkParams(body)
   let response
-  if(!model) errors.push('model is required')
-  // if(!colors) errors.push('color is required')
-  // if(!size) errors.push('size is required')
-  // if(!room) errors.push('room is required')
 
   if (errors.length > 0) response = {errors}
   else {
-    const curtain = { id: uuid(), model, color }
-    //, size, room
+    const curtain = { id: uuid(), model, color, size, room }
     curtains.push(curtain)
     response = curtain
   }
-  // if(!model) {
-  //   errors.push('model is required')
-  //   response = {errors}
-  // } else if(!color) {
-  //   errors.push('color is required')
-  //   response = {errors}
-
   return response
 }
 
 function getAll(curtains){
+    console.log(curtains);
     return curtains
 }
 
@@ -47,5 +30,26 @@ function checkId(req, res, next) {
   if(!match) return next({status: 404, message: `Could not find item with the id ${id}`})
 }
 
+function checkParams(body) {
+  const errors = []
+
+  const model = body.model
+  const color = body.color
+  const size = body.size
+  const room = body.room
+
+  if(!model) errors.push('model is required')
+  if(!color) errors.push('color is required')
+  if(!size) errors.push('size is required')
+  if(!room) errors.push('room is required')
+
+  return errors
+}
+
+// function createItem() {
+//   const item = { id: uuid(), model, color, size, room }
+//   curtains.push(curtain)
+//   response = curtain
+// }
 
 module.exports = {getAll, create, checkId}
