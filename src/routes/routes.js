50c3b457747a9df70e5638d4392dff2736b53487
Lena Venable <venablena@gmail.com>
2017-10-22 17:36:38 -0700

    create routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-//const ctrl = require('../controllers/cats')
+const ctrl = require('../controllers/controllers')
 
-// router.get('/', ctrl.getAll)
-// router.post('/', ctrl.create)
+router.get('/', ctrl.getAll)
+router.post('/', ctrl.create)
 
 module.exports = router
